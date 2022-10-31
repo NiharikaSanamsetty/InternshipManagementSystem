77c2d54c09c9249ed13a141f6fc22807e196d1bb
sainarenchava <S544921@NWMISSOURI.EDU>
2022-10-31 08:33:51 -0500

    Working on the bugs in the faculty dashboard as some clicks were not being registered

--- InternshipManagementSystem/ViewControllers/Faculty/FacultyDashBoard.swift
+++ InternshipManagementSystem/ViewControllers/Faculty/FacultyDashBoard.swift
@@ -23,6 +23,30 @@ class FacultyDashBoard: UIViewController , UITableViewDelegate , UITableViewData
   }
    
    
+   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       return true
+   }
+   
+   func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+      
+       if let text = textField.text,
+                 let textRange = Range(range, in: text) {
+                 let updatedText = text.replacingCharacters(in: textRange,
+                                                             with: string)
+           
+           if(updatedText.isEmpty) {
+               self.searchTextField.text = ""
+               self.onSearch(UIButton())
+           }else  {
+              // self.onSearch(UIButton())
+               
+           }
+           
+       }
+       
+       return true
+   
+   }
 
   @IBAction func onSearch(_ sender: Any) {
 
@@ -46,7 +70,22 @@ class FacultyDashBoard: UIViewController , UITableViewDelegate , UITableViewData
         
     }
 
-  
+  func performSearch() {
+
+      var temp =  [StudentFormData]()
+      
+      for snap in tempSnapshot {
+          
+          let company = snap.company!.lowercased()
+          
+          if(company.contains(self.searchTextField.text!.lowercased())) {
+              temp.append(snap)
+          }
+      }
+      self.filterSnapShot.removeAll()
+      self.filterSnapShot = temp
+      self.tableView.reloadData()
+  }
 
   override func viewWillAppear(_ animated: Bool) {
       self.getApplications()
@@ -61,7 +100,20 @@ class FacultyDashBoard: UIViewController , UITableViewDelegate , UITableViewData
   }
 
 
- 
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+      let cell = self.tableView.dequeueReusableCell(withIdentifier: "ApplicationStatusCellFaculty") as! ApplicationStatusCellFaculty
+      let data = self.filterSnapShot[indexPath.row]
+      cell.copyButton.tag = indexPath.row
+      cell.attchmentButton.tag = indexPath.row
+      cell.copyButton.addTarget(self, action: #selector(copyButtonTapped(_:)), for: .touchUpInside)
+      cell.attchmentButton.addTarget(self, action: #selector(showAttchment(_:)), for: .touchUpInside)
+      cell.setData(data: data)
+
+
+      return cell
+  }
+
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
       let id =  self.filterSnapShot[indexPath.row].id
@@ -122,8 +174,53 @@ class FacultyDashBoard: UIViewController , UITableViewDelegate , UITableViewData
       toast.show()
   }
    
+   @objc func showAttchment(_ sender: UIButton){
+
+       
+       let vc = self.storyboard?.instantiateViewController(withIdentifier: "PreViewVC") as! PreViewVC
+       vc.attachments = self.filterSnapShot[sender.tag].uploadFileList!
+       self.navigationController?.pushViewController(vc, animated: true)
+       
+   
+   }
+
 
    
+   
+  func getApplications() {
+
+      self.tempSnapshot.removeAll()
+
+      
+        FireStoreManager.shared.getApplicationFourmsByDepartment(department: UserDefaultsManager.shared.getDepartment()) { querySnapshot in
+
+          
+          var itemsArray = [StudentFormData]()
+          
+          for (_,document) in querySnapshot.documents.enumerated() {
+              do {
+                  let item = try document.data(as: StudentFormData.self)
+                  itemsArray.append(item)
+                  
+                  print(itemsArray.count)
+               
+              }catch let error {
+                  print(error)
+              }
+          }
+          
+          
+          self.tempSnapshot = itemsArray
+          self.filterSnapShot = itemsArray
+          self.tableView.reloadData()
+      
+       
+
+      }
+}
+
+
+}
 
 
 
@@ -186,6 +283,71 @@ extension UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
 }
-   
- 
+
+
+
+
+extension FacultyDashBoard {
+    
+    func searchByStatus(status:String) {
+        
+        
+        var temp =  [StudentFormData]()
+        
+        for snap in tempSnapshot {
+            
+            let company = snap.status!
+            
+            if(company.contains(status)) {
+                temp.append(snap)
+            }
+        }
+        self.filterSnapShot.removeAll()
+        self.filterSnapShot = temp
+        self.tableView.reloadData()
+        
+    }
+    
+    
+    @IBAction func onFilter(_ sender: Any) {
+        
+        let dialog = SelectionDialog(title: "Please Select Status", closeButtonTitle: "Close")
+        
+        dialog.addItem(item: "Pending", icon: UIImage(named: "pending")!) {
+             
+            dialog.close()
+            self.searchByStatus(status: "Pending")
+            
+        }
+        
+        dialog.addItem(item: "Approved", icon: UIImage(named: "approved")!) {
+           
+            dialog.close()
+            self.searchByStatus(status: "Approved")
+            
+           
+        }
+        
+        
+        dialog.addItem(item: "Rejected", icon: UIImage(named: "rejected")!) {
+            
+            dialog.close()
+            self.searchByStatus(status: "Rejected")
+          
+        }
+        
+        
+        dialog.addItem(item: "All", icon: UIImage(named: "copy2")!) {
+           
+            dialog.close()
+            
+            self.filterSnapShot = self.tempSnapshot
+            self.tableView.reloadData()
+           
+        }
+        
+        
+        
+        dialog.show()
+    }
 }
